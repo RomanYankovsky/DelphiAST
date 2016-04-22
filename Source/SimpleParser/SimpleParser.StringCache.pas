fc0470b471697c05e9391c658ee8953c59d5279f
David Millington <vintagedave@gmail.com>
2016-04-22 17:27:30 +0300

    Basic threadsafety for the string cache. Not extensively reviewed. When threadsafe, any individual instance has get/add method contents wrapped in a lock, to lock the internal structures. The instance is never cleared when the count of objects using it drops to 0, since that's a bit more complex to get right (should only clear while the refcount is 0, but have to lock that to ensure it's not changed while clearing is happening, which severely slows inc/decrementing the count. I couldn't think of a good compare-lock-exchange algorithm. So just don't bother; it's never cleared while alive, when threadsafe.)

--- Source/SimpleParser/SimpleParser.StringCache.pas
+++ Source/SimpleParser/SimpleParser.StringCache.pas
@@ -3,7 +3,14 @@ unit SimpleParser.StringCache;
 interface
 
 uses
-  System.Generics.Defaults, System.Generics.Collections;
+  System.Generics.Defaults, System.Generics.Collections, SyncObjs;
+
+// Use STRINGCACHE_THREADSAFE to ensure one instance can be accessed by multiple
+// threads at once. This prevents clearing - it keeps all added elements for the
+// life of the instance (life of the program if using TStringCache.Instance)
+// and locks around adding / getting items.
+// This is one by default
+{$define STRINGCACHE_THREADSAFE}
 
 type
   TStringId = type NativeInt;
@@ -36,13 +43,23 @@ type
   strict private
     FStringToId : TDictionary<TStringRec, TStringId>;
     FRefCount : NativeInt;
-    FIsPersistent : Boolean;
+    {$ifdef STRINGCACHE_THREADSAFE}
+      FLock : TCriticalSection;
+    {$else}
+      // If threadsafe, always persistent
+      FIsPersistent : Boolean;
+    {$endif}
 
     class var FInstance : TStringCache;
     class constructor ClassCreate;
     class destructor ClassDestroy;
+
+    procedure Lock; inline;
+    procedure Unlock; inline;
   private
-    FIdToString : TList<TStringRec>; // ID is index
+    FIdToString : TList<TStringRec>;
+    function GetIsPersistent: Boolean;
+    procedure SetIsPersistent(const Value: Boolean); // ID is index
   public
     constructor Create;
     destructor Destroy; override;
@@ -50,13 +67,13 @@ type
     function Add(const Value : string) : TStringId;
     function AddAndGet(const P : PChar; const Length : Integer) : string;
     function Get(const ID : TStringId) : string;
-    procedure Clear;
+    procedure Clear(const OnDestruction : Boolean = false);
     procedure ByUsage(InOrder : TList<TStringRec>);
 
     procedure IncRef;
     procedure DecRef;
 
-    property Persistent : Boolean read FIsPersistent write FIsPersistent;
+    property Persistent : Boolean read GetIsPersistent write SetIsPersistent;
     class property Instance : TStringCache read FInstance;
   end;
 
@@ -116,7 +133,11 @@ constructor TStringCache.Create;
 begin
   inherited;
   FRefCount := 0;
-  FIsPersistent := false; // Clear the cache when no longer needed
+  {$ifdef STRINGCACHE_THREADSAFE}
+    FLock := TCriticalSection.Create;
+  {$else}
+    FIsPersistent := false; // Clear the cache when no longer needed
+  {$endif}
   FStringToId := TDictionary<TStringRec, TStringId>.Create(
     TStringCache.TStringRecValueEqualityComparer.Create);
   FIdToString := TList<TStringRec>.Create;
@@ -127,9 +148,12 @@ end;
 destructor TStringCache.Destroy;
 begin
   assert(FRefCount = 0, 'String cache destroyed with live objects still relying on it');
-  Clear;
+  Clear(true);
   FStringToId.Free;
   FIdToString.Free;
+  {$ifdef STRINGCACHE_THREADSAFE}
+    FLock.Free;
+  {$endif}
   inherited;
 end;
 
@@ -140,16 +164,21 @@ begin
   Result := 0;
   Item := TStringRec.Create(Value);
 
-  if FStringToId.TryGetValue(Item, Result) then begin
-    // Already exists. Increment the usage count of the existing one, and return
-    FIdToString[Result].IncUsageCount;
-    Item.Free; // Already exists, Item was search key only
-    Exit;
-  end;
+  Lock;
+  try
+    if FStringToId.TryGetValue(Item, Result) then begin
+      // Already exists. Increment the usage count of the existing one, and return
+      FIdToString[Result].IncUsageCount;
+      Item.Free; // Already exists, Item was search key only
+      Exit;
+    end;
 
-  // Item does not yet exist
-  Result := FIdToString.Add(Item);
-  FStringToId.Add(Item, Result);
+    // Item does not yet exist
+    Result := FIdToString.Add(Item);
+    FStringToId.Add(Item, Result);
+  finally
+    Unlock;
+  end;
 end;
 
 function TStringCache.AddAndGet(const P : PChar; const Length : Integer) : string;
@@ -162,16 +191,23 @@ end;
 
 function TStringCache.Get(const ID: TStringId): string;
 begin
-  if ID < FIdToString.Count then
-    Exit(FIdToString[ID].Value)
-  else
-    raise Exception.Create(Format('String cache entry with ID %d does not exist', [ID]));
+  Lock;
+  try
+    if ID < FIdToString.Count then
+      Exit(FIdToString[ID].Value)
+    else
+      raise Exception.Create(Format('String cache entry with ID %d does not exist', [ID]));
+  finally
+    Unlock;
+  end;
 end;
 
-procedure TStringCache.Clear;
+procedure TStringCache.Clear(const OnDestruction : Boolean);
 var
   I : Integer;
 begin
+  // This doesn't need a lock. When threadsafe, never cleared except on destruction
+
   if FRefCount <> 0 then
     raise Exception.Create(Format('Clearing the string cache while objects still rely on it (%d)', [FRefCount]));
 
@@ -181,12 +217,41 @@ begin
 
   FStringToId.Clear;
   FIdToString.Clear;
+
+  if not OnDestruction then begin
+    // Add emtpy string - it's always item 0 - unless this is being called as
+    // part of destruction
+    Add('');
+    assert(Get(0) = '');
+  end;
 end;
 
 procedure TStringCache.ByUsage(InOrder: TList<TStringRec>);
 begin
-  InOrder.InsertRange(0, FIdToString);
-  InOrder.Sort(TStringCache.TStringRecUsageComparer.Create);
+  Lock;
+  try
+    InOrder.InsertRange(0, FIdToString);
+    InOrder.Sort(TStringCache.TStringRecUsageComparer.Create);
+  finally
+    Unlock;
+  end;
+end;
+
+function TStringCache.GetIsPersistent: Boolean;
+begin
+  {$ifdef STRINGCACHE_THREADSAFE}
+    Result := true; // Never clears
+  {$else}
+    Result := FPersistent;
+  {$endif}
+end;
+
+procedure TStringCache.SetIsPersistent(const Value: Boolean);
+begin
+  // If threadsafe, always persistent (never clears) so don't set anything
+  {$ifndef STRINGCACHE_THREADSAFE}
+    FPersistent := Value;
+  {$endif}
 end;
 
 procedure TStringCache.IncRef;
@@ -196,20 +261,41 @@ begin
   // they are no longer needed. (The alternative, controlled by Persistent,
   // is to keep them - ie make the cache persistent over multiple runs - useful
   // for parsing the same or similar files over and over.)
-  Inc(FRefCount);
+  AtomicIncrement(FRefCount);
 end;
 
 procedure TStringCache.DecRef;
 begin
-  if FRefCount = 0 then
+  if AtomicDecrement(FRefCount) < 0 then
     raise Exception.Create('String cache refcount cannot be decremented below zero');
-  Dec(FRefCount);
 
-  // Unless want to keep the strings around for next parse, clear now nothing is
-  // using any of them.
-  if (FRefCount = 0) and (not FIsPersistent) then
-    Clear;
+  // When threadsafe, synchronizing clearing while ensuring the refcount is 0
+  // (ie an addref dosn't occur while clearing) is hard without locking around
+  // IncRef and DecRef, which is expensive. So just don't clear.
+  {$ifndef STRINGCACHE_THREADSAFE}
+    // Unless want to keep the strings around for next parse, clear now nothing is
+    // using any of them.
+    if (FRefCount = 0) and (not Persistent) then
+      Clear;
+  {$endif}
 end;
+
+procedure TStringCache.Lock;
+begin
+  // If not threadsafe, nothing to do here
+  {$ifdef STRINGCACHE_THREADSAFE}
+    FLock.Acquire;
+  {$endif}
+end;
+
+procedure TStringCache.Unlock;
+begin
+  // If not threadsafe, nothing to do here
+  {$ifdef STRINGCACHE_THREADSAFE}
+    FLock.Release;
+  {$endif}
+end;
+
 
 { TStringCache.TStringRec }
 
