81f61d9efd5c58c445d26bb916f4a3b863b8935d
Primoz Gabrijelcic <primoz@gabrijelcic.org>
2017-07-25 14:20:17 +0200

    moved to correct place

--- Source/DelphiAST.Serialize.Binary.pas
+++ Source/DelphiAST.Serialize.Binary.pas
@@ -313,8 +313,11 @@ begin
       Exit;
   end
   else begin
-    if Length(S) > 4 then
+    if Length(S) > 4 then begin
       FStringTable.Add(S, FStringTable.Count);
+      if FStringTable.Count > $FFFFFF then
+        raise Exception.Create('TBinarySerializer.WriteString: Too many strings!');
+    end;
     u8 := UTF8Encode(s);
     i := Length(u8);
     if not WriteNumber(i) then
