7682a4aafc5cad0a04725d8cfd13eca5121f1369
Primoz Gabrijelcic <primoz@gabrijelcic.org>
2017-07-26 09:28:50 +0200

    wip

--- Project indexer/ProjectIndexer.pas
+++ Project indexer/ProjectIndexer.pas
@@ -2,10 +2,19 @@ unit ProjectIndexer;
 
 interface
 
+uses
+  System.Classes, System.Generics.Collections,
+  DelphiAST, DelphiAST.Classes, DelphiAST.Consts;
+
 type
   TProjectIndexer = class
+  strict private
+    FParsedUnits: TDictionary<string,TSyntaxNode>;
   strict protected
-    procedure ParseUnit(const fileName: string);
+    procedure AppendUnits(usesNode: TSyntaxNode; unitList: TStrings);
+    procedure BuildUsesList(unitNode: TSyntaxNode; isProject: boolean; unitList: TStringList);
+    function FindType(node: TSyntaxNode; nodeType: TSyntaxNodeType): TSyntaxNode;
+    procedure ParseUnit(const fileName: string; isProject: boolean);
   public
     procedure Index(const fileName: string);
   end;
@@ -13,20 +22,78 @@ type
 implementation
 
 uses
-  DelphiAST, DelphiAST.Classes;
+  System.SysUtils;
 
 { TProjectIndexer }
 
-procedure TProjectIndexer.Index(const fileName: string);
+procedure TProjectIndexer.AppendUnits(usesNode: TSyntaxNode; unitList: TStrings);
+var
+  childNode: TSyntaxNode;
 begin
-  ParseUnit(fileName);
+  for childNode in usesNode.ChildNodes do
+    if childNode.Typ = ntUnit then
+      unitList.Add(childNode.GetAttribute(anName)); // 'path' is currently ignored
 end;
 
-procedure TProjectIndexer.ParseUnit(const fileName: string);
+procedure TProjectIndexer.BuildUsesList(unitNode: TSyntaxNode; isProject: boolean;
+  unitList: TStringList);
 var
-  SyntaxTree: TSyntaxNode;
+  implNode: TSyntaxNode;
+  intfNode: TSyntaxNode;
+  usesNode: TSyntaxNode;
 begin
-  SyntaxTree := TPasSyntaxTreeBuilder.Run(fileName, false);
+  if isProject then begin
+    usesNode := FindType(unitNode, ntUses);
+    if assigned(usesNode) then
+      AppendUnits(usesNode, unitList);
+  end
+  else begin
+    intfNode := FindType(unitNode, ntInterface);
+    if assigned(intfNode) then begin
+      usesNode := FindType(intfNode, ntUses);
+      if assigned(usesNode) then
+        AppendUnits(usesNode, unitList);
+    end;
+    implNode := FindType(unitNode, ntImplementation);
+    if assigned(implNode) then begin
+      usesNode := FindType(implNode, ntUses);
+      if assigned(usesNode) then
+        AppendUnits(usesNode, unitList);
+    end;
+  end;
+end;
+
+function TProjectIndexer.FindType(node: TSyntaxNode; nodeType: TSyntaxNodeType):
+  TSyntaxNode;
+begin
+  if node.Typ = nodeType then
+    Exit(node)
+  else
+    Result := node.FindNode(nodeType);
+end;
+
+procedure TProjectIndexer.Index(const fileName: string);
+begin
+  FParsedUnits := TDictionary<string,TSyntaxNode>.Create;
+  ParseUnit(fileName, true);
+  FParsedUnits.Free;
+end;
+
+procedure TProjectIndexer.ParseUnit(const fileName: string; isProject: boolean);
+var
+  syntaxTree: TSyntaxNode;
+  unitList  : TStringList;
+  unitNode  : TSyntaxNode;
+begin
+  syntaxTree := TPasSyntaxTreeBuilder.Run(fileName, false);
+  unitNode := FindType(syntaxTree, ntUnit);
+  if assigned(unitNode) then begin
+    unitList := TStringList.Create;
+    try
+      BuildUsesList(unitNode, isProject, unitList);
+
+    finally FreeAndNil(unitList); end;
+  end;
 end;
 
 end.
