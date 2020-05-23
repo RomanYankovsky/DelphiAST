207d69cff6241a9f1e700f30f1c1ffa6169bd909
Roman Yankovsky <roman@yankovsky.me>
2020-05-23 22:48:05 +0300

    Better unicode support in identifiers (fix #302)

--- Source/SimpleParser/SimpleParser.Lexer.pas
+++ Source/SimpleParser/SimpleParser.Lexer.pas
@@ -1878,11 +1878,10 @@ end;
 
 function TmwBasePasLex.IsIdentifiers(AChar: Char): Boolean;
 begin
-{$IFDEF SUPPORTS_INTRINSIC_HELPERS}
-  Result := (AChar.IsLetterOrDigit) or (AChar = '_');
-{$ELSE}
-  Result := TCharacter.IsLetterOrDigit(AChar) or (AChar = '_');
-{$ENDIF}
+  // assuming Delphi identifier may include letters, digits, underscore symbol
+  // and any character over 127 except surrogates
+  Result := TCharacter.IsLetterOrDigit(AChar) or (AChar = '_')
+    or ((Ord(AChar) > 127) and not TCharacter.IsHighSurrogate(AChar) and not TCharacter.IsLowSurrogate(AChar));
 end;
 
 procedure TmwBasePasLex.LFProc;
